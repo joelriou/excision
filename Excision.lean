b273fa816eddf439d90f6f3e6bae708c9e3d4313
Joël Riou <joel.riou@universite-paris-saclay.fr>
2026-07-19 09:13:55 +0200

    wip

--- Excision.lean
+++ Excision.lean
@@ -1,5 +1,6 @@
 import Excision.ConvexSpace.AffineMap
 import Excision.ConvexSpace.StdSimplex
 import Excision.ConvexSpace.ToSSet
+import Excision.Linear.ULift
 import Excision.SimplexCategory.Basic
 import Excision.SimplicialSet.ChainComplexAb
---
+++ Excision/Linear/ULift.lean
@@ -0,0 +1,35 @@
+/-
+Copyright (c) 2026 Joël Riou. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Joël Riou
+-/
+module
+
+public import Mathlib
+public import Mathlib.Algebra.Ring.ULift
+public import Mathlib.CategoryTheory.Linear.Basic
+
+/-!
+# Cochains of a simplicial set with coefficients in an abelian group
+
+-/
+
+universe v u
+
+@[expose] public section
+
+namespace CategoryTheory.Linear
+
+variable {R : Type u} [Semiring R] {C : Type*} [Category* C] [Preadditive C]
+  [Linear R C]
+
+instance ulift : Linear (ULift.{v} R) C where
+  homModule _ _ := Module.compHom _ ULift.ringEquiv.toRingHom
+  smul_comp := fun _ _ _ ⟨r⟩ f g ↦ Linear.smul_comp _ _ _ r f g
+  comp_smul := fun _ _ _ f ⟨r⟩ g ↦ Linear.comp_smul _ _ _ f r g
+
+@[simp]
+lemma uliftUp_smul {X Y : C} (r : R) (f : X ⟶ Y) :
+    ULift.up.{v} r • f = r • f := rfl
+
+end CategoryTheory.Linear
